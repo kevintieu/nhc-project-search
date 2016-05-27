f82054da1d66635a0cda77b6b5a28de4de416b0a
Kevin Tieu <kevin.tieu@hotmail.com>
2016-05-27 16:56:34 -0700

    added javascript file

---
+++ scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scroll').fadeIn();
+		} else {
+			$('.scroll').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scroll').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+	$('[data-toggle="tooltip"]').tooltip();
+
+});

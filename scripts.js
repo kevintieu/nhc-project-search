439db7076a8665238c33541f3c34ed0fd7658700
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-02 17:01:33 -0700

    added edit/save button to table

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	
 	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
+	$(window).scroll(function() {
 		if ($(this).scrollTop() > 100) {
 			$('.scroll').fadeIn();
 		} else {
@@ -10,11 +10,29 @@ $(document).ready(function(){
 	});
 	
 	//Click event to scroll to top
-	$('.scroll').click(function(){
+	$('.scroll').click(function() {
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
 	
 	$('[data-toggle="tooltip"]').tooltip();
+
+    $('.edit-btn').click(function() {
+      	var currentTD = $(this).parents('tr').find('td');
+      	if ($(this).html() == 'Edit') {
+        	currentTD = $(this).parents('tr').find('td');
+        	$.each(currentTD, function() {
+          		$(this).prop('contenteditable', true)
+          		$(this).addClass('editable')
+        	});	
+      	} else {
+        	$.each(currentTD, function() {
+          		$(this).prop('contenteditable', false)
+          		$(this).removeClass('editable')
+        	});
+      	}
+
+      	$(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
+    });
 
 });

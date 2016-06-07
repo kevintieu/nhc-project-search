8425992eca8eb26c6ca5f318947af3dc51134ab1
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-07 15:51:34 -0700

    added editing/saving functionality to the edit button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function() {
 		if ($(this).scrollTop() > 100) {
@@ -17,22 +17,55 @@ $(document).ready(function() {
 	
 	$('[data-toggle="tooltip"]').tooltip();
 
-    $('.edit-btn').click(function() {
-      	var currentTD = $(this).parents('tr').find('td');
-      	if ($(this).html() == 'Edit') {
-        	currentTD = $(this).parents('tr').find('td');
-        	$.each(currentTD, function() {
-          		$(this).prop('contenteditable', true)
-          		$(this).addClass('editable')
-        	});	
-      	} else {
-        	$.each(currentTD, function() {
-          		$(this).prop('contenteditable', false)
-          		$(this).removeClass('editable')
-        	});
-      	}
+  $('.edit-btn').on('click', function() {
+    $currentTD = $(this).parents('tr').find('td');
+    if($(this).val() == 'Edit') {
+      $(this).val('Save');
+      $.each($currentTD, function() {
+        if($(this).prop('class') == 'xx') {
+          return false;
+        }
+        $(this).on('blur', function() {
+          $.each($currentTD, function() {
+            $(this).prop('contenteditable', false);
+            $(this).removeClass('editable');
+            $('.edit-btn').val('Edit');
+          });
+        });
+        $(this).prop('contenteditable', true);
+        $(this).addClass('editable');
+      });
+      return false;
+    } else if($(this).val() == 'Save') {
+      $(this).val('Edit');
+      $.each($currentTD, function() {
+        if($(this).prop('class') == 'xx') {
+          return false;
+        }
+        $(this).prop('contenteditable', false);
+        $(this).removeClass('editable');
+        if($(this).prop('class') == 'proj_cd') {
+          $proj_cd = $(this).prop('id');
+        }
+        $field_name = $(this).prop('class');
+        $value = $(this).html();
+        $.ajax({
+          url: 'edit.php',
+          type: 'POST',
+          data: {
+            field_name: $field_name,
+            value: $value,
+            proj_cd: $proj_cd
+          },
+          success: function(result) {
+            console.log(result);
+          }
+        });
+      });
+      return false;
+    }
+  });
 
-      	$(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
-    });
 
 });
+

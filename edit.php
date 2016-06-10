3315769690318e132ce9c1baf0cf951b74634181
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-10 14:27:04 -0700

    added logic for sanitizing data

--- edit.php
+++ edit.php
@@ -1,9 +1,20 @@
 <?php
 	include 'LoadDb.php';
 
+	function sanitize($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
 	$proj_cd = $_POST['proj_cd'];
 	$field_name = $_POST['field_name'];
 	$value = $_POST['value'];
+
+	sanitize($proj_cd);
+	sanitize($field_name);
+	sanitize($value);
 
 	$result = pg_query("UPDATE proj SET " . $field_name . "='" . $value . "' WHERE proj_cd='" . $proj_cd ."'") or die('Could not execute query.');
 ?>

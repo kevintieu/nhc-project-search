c718da8dc95f06f630cc1063f6a3bdf63b3d3a9f
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-10 14:59:18 -0700

    added alerts for saving and deleting records, added a modal for inputting records, added icons to action buttons

--- ProjSearchNew_V3.php
+++ ProjSearchNew_V3.php
@@ -79,12 +79,72 @@
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<link rel="stylesheet" href="styles.css" type="text/css" >
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<script src="https://use.fontawesome.com/5a59419cf4.js"></script>
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
   		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   		<script src="scripts.js" type="text/javascript"></script>
 	</head>
 
 	<body class="main">
+		<div class='container'>
+			<div class='modal fade' id='new_entry' role='dialog'>
+				<div class='modal-dialog modal-lg'>
+					<div class='modal-content'>
+						<div class='modal-header'>
+							<button type='button' class='close' data-dismiss='modal'>&times;</button>
+							<h4 class='modal-title'>New Entry</h4>
+						</div>
+						<div class='modal-body'>
+							<form role='form' method='POST' action='' id='new_entry_form'>
+								<div class='form-group'>
+									<label for='proj_cd'>Project Number: </label>
+									<input type='text' class='form-control' id='proj_cd_form'>
+								</div>
+								<div class='form-group'>
+									<label for='proj_nm'>Project Name: </label>
+									<input type='text' class='form-control' id='proj_nm_form'>
+								</div>
+								<div class='form-group'>
+									<label for='proj_mgr'>Project Manager: </label>
+									<input type='text' class='form-control' id='proj_mgr_form' placeholder='Last Name, First Name'>
+								</div>
+								<div class='form-group'>
+									<label for='client_nm'>Client Name: </label>
+									<input type='text' class='form-control' id='client_nm_form'>
+								</div>
+								<div class='form-group'>
+									<label for='proj_loc'>Project Location: </label>
+									<input type='text' class='form-control' id='proj_loc_form'>
+								</div>
+							</form>
+						</div>
+						<div class='modal-footer'>
+							<button type='button' class='btn btn-success' data-dismiss='modal' id='insert'>Insert</button>
+							<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--
+			<div class='modal fade' id='delete_confirm' role='dialog'>
+				<div class='modal-dialog modal-sm'>
+					<div class='modal-content'>
+						<div class='modal-header'>
+							<button type='button' class='close' data-dismiss='modal'>&times;</button>
+							<h4 class='modal-title'>Warning</h4>
+						</div>
+						<div class='modal-body'>
+							<p>Are you sure you want to delete this row?</p>
+						</div>
+						<div class='modal-footer center-block'>
+							<button type='button' class='btn btn-success' id='delete_yes' data-dismiss='modal'>Yes</button>
+							<button type='button' class='btn btn-danger' id='delete_no' data-dismiss='modal'>No</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			-->
+		</div>
 		<div class="container-fluid search-bar">
 			<div class="row">
 				<form role="form" action="ProjSearchNew_V3.php" method="POST">
@@ -97,13 +157,12 @@
 					<div class="col-xs-8">
 						<div class="row">
 							<div class="form-group col-xs-11">
-								<input type="text" class="form-control" value="<?php echo htmlspecialchars($_POST['input']); ?>" name="input" placeholder="Enter a keyword">		
+								<input type="text" class="form-control" value="<?php echo htmlspecialchars($_POST['input']); ?>" name="input" placeholder="Search by project number or keyword">		
 							</div>
 							<div class="form group col-xs-1">
 								<button type="submit" class="btn btn-primary" name="search" id="search" value="Search">Search</button>
 							</div>
 						</div>
-
 					</div>
 					<div class="col-xs-2">
 						<button type="submit" class="btn btn-danger center-block" name="logout" value="Log-Out">Logout</button>
@@ -114,7 +173,7 @@
 				<div class="col-xs-2">
 					<?php 
 					if($permissions == 't') {
-						echo "<button type='button' class='btn btn-success btn-xs center-block'>New Entry</button>";
+						echo "<button type='button' class='btn btn-success btn-xs center-block' data-toggle='modal' data-target='#new_entry'>New Entry</button>";
 					} else {
 						echo "<button type='button' class='btn btn-success btn-xs center-block disabled' data-toggle='tooltip' data-placement='bottom' title='You do not have permission'>New Entry</button>";
 					}
@@ -160,18 +219,19 @@
 										<td class='proj_loc' id='proj_loc'>" . $rows['proj_loc'] . "</td>
 										<td class='xx'>
 											<form method='POST' action=''>
-												<div class='btn-group btn-group-xs'>
-													";
+												<div class='btn-group btn-group-xs'>";
 													if($permissions == 't') {
 														echo "
-															<button type='button' class='btn btn-info edit-btn'>Edit</button>
-															<button type='button' class='btn btn-info save-btn'>Save</button>
-															<button type='button' class='btn btn-info delete-btn'>Delete</button>";
+															<button type='button' class='btn btn-success edit-btn' value='Edit'><span><i class='fa fa-pencil fa-lg' aria-hidden='true'></i></span></button>
+															<button type='button' class='btn btn-success save-btn' value='Save'><i class='fa fa-floppy-o fa-lg' aria-hidden='true'></i></button>
+															<button type='button' class='btn btn-success delete-btn' value='Delete' data-toggle='modal' data-target='#delete_confirm'><i class='fa fa-trash fa-lg' aria-hidden='true'></i></button>
+															";
 													} else {
 														echo "
-															<button type='button' class='btn btn-info edit-btn disabled'>Edit</button>
-															<button type='button' class='btn btn-info save-btn disabled'>Save</button>
-															<button type='button' class='btn btn-info delete-btn disabled'>Delete</button>";
+															<button type='button' class='btn btn-success edit-btn disabled' disabled='disabled' value='Edit'><span><i class='fa fa-pencil fa-lg' aria-hidden='true'></i></span></button>
+															<button type='button' class='btn btn-success save-btn disabled' disabled='disabled' value='Save'><i class='fa fa-floppy-o fa-lg' aria-hidden='true'></i></button>
+															<button type='button' class='btn btn-success delete-btn disabled' disabled='disabled' value='Delete' data-toggle='modal' data-target='#delete_confirm'><i class='fa fa-trash fa-lg' aria-hidden='true'></i></button>
+															";
 													}
 													echo "
 												</div>

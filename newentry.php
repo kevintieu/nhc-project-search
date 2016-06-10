79ed1d656f08d46f2ebc44ec7d26258a73ef091f
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-10 14:26:00 -0700

    added logic for posting form data using Ajax

---
+++ newentry.php
@@ -0,0 +1,26 @@
+<?php
+  include 'LoadDb.php';
+
+  function sanitize($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $proj_cd_form = $_POST['proj_cd_form'];
+  $proj_nm_form = $_POST['proj_nm_form'];
+  $proj_mgr_form = $_POST['proj_mgr_form'];
+  $client_nm_form = $_POST['client_nm_form'];
+  $proj_loc_form = $_POST['proj_loc_form'];
+
+  sanitize($proj_cd);
+  sanitize($proj_nm);
+  sanitize($proj_mgr);
+  sanitize($client_nm);
+  sanitize($proj_loc);
+
+  $result = pg_query("INSERT INTO proj (proj_cd, proj_nm, proj_mgr, client_nm, proj_loc) 
+  VALUES ('$proj_cd_form', '$proj_nm_form', '$proj_mgr_form', '$client_nm_form', '$proj_loc_form')"); 
+
+?>

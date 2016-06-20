6a515be88c523301ad9c24a95122b075a17fc8d8
Kevin Tieu <kevin.tieu@hotmail.com>
2016-06-20 10:48:57 -0700

    updated + renamed file

--- login_security.php
+++ login_security.php
@@ -3,6 +3,6 @@
 
     if (!(isset($_SESSION['login']) AND $_SESSION['login'] != '')) {
         // exit program and return to home page
-        header("Location: indexnew.php");
+        header("Location: index.php");
 	}
 ?>
